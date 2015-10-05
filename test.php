c646d3e3438cc707f0fa353a922649ff9c1ce787
s <s@s.com>
2015-10-05 09:58:16 +0300

    Востановелно

--- test.php
+++ test.php
@@ -1,2 +1,11 @@
-<?php
-echo 'Hello world!';
+<html>
+    <head>
+        <title>TEST</title>
+        <link type="text/css" rel="stylesheet" href="test.css">
+    </head>
+    <body>
+        <input type="checkbox" value="value1">
+    </body>
+</html>
+
+
